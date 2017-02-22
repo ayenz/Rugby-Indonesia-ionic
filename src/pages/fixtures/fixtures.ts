f14de15c34a2e4ef9b8c6d174e242faffa11f157
Stanley Hanes <Stanley Hanes>
2017-02-22 12:31:15 +0700

    add json

--- src/pages/fixtures/fixtures.ts
+++ src/pages/fixtures/fixtures.ts
@@ -1,38 +1,22 @@
 import { Component } from '@angular/core';
+import { Http, Response} from '@angular/http';
+import 'rxjs/add/operator/map';
 
-import { NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-fixtures',
   templateUrl: 'fixtures.html'
 })
 export class Fixtures {
-  selectedItem: any;
+  posts: any[];
   icons: string[];
   items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(Fixtures, {
-      item: item
-    });
+  constructor(public http: Http) {
+    this.http.get('https://ri-admin.azurewebsites.net/indonesianrugby/fixtures/lista.json/').map((res:Response) => res.json()).subscribe((data) => {
+		console.log(data);
+		this.posts = data;
+	})
+	//console.log(post[1]);
   }
 }
