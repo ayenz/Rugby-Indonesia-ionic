5413ec6f7ae7c18a7e04960b63ebb13c29a641ab
Kresnadc <kresna_dcahyo@yahoo.co.id>
2017-02-28 14:59:17 +0700

    Home Page rugby101

--- src/pages/rugby101/rugby101.ts
+++ src/pages/rugby101/rugby101.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-
+import { News } from '../news/news';
 
 @Component({
   selector: 'page-rugby101',
   templateUrl: 'rugby101.html'
 })
 export class Rugby101 {
-
+  homePage = News;
   constructor() {
 
   }
